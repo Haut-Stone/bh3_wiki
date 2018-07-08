0514cdd889fb108ea43ce05763e25dabeb987023
SJH <haut.1604.stone@gmail.com>
2018-07-08 22:46:16 +0800

    替换reset.css为原来的reset.css，因为代码规范中的reset.css有一些显示问题

--- src/pages/Stigmata/Stigmata.vue
+++ src/pages/Stigmata/Stigmata.vue
@@ -2,17 +2,22 @@
   <div class="stigmata">
     <h1 v-if="$route.params.star">{{ $route.params.star }}</h1>
     <h1 v-else>{{ $route.params.id }}</h1>
+    <NavMenu></NavMenu>
     <router-view/>
   </div>
 </template>
 
 <script>
+import NavMenu from '@/components/NavMenu'
 export default {
   name: 'Stigmata',
   data () {
     return {
       msg: '圣痕导航'
     }
+  },
+  components: {
+    NavMenu
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -35,7 +35,7 @@ export default new Router({
       ]
     },
     {
-      path: '/sitgmata',
+      path: '/stigmata',
       name: 'Stigmata',
       component: Stigmata,
       children: [
