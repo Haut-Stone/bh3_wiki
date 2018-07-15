93650aaa25e1ca447240b4c9fc473692de7af3cf
SJH <haut.1604.stone@gmail.com>
2018-07-15 19:38:47 +0800

    添加了首页的样式

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
-
+import * as snowFall from './common/js/snowfall'
 // 引入echarts
 import echarts from 'echarts'
 
@@ -13,6 +13,7 @@ import 'amfe-flexible'
 
 Vue.prototype.$echarts = echarts
 Vue.prototype.$http = axios
+Vue.prototype.$snowFall = snowFall
 // 上线部署主机域名
 Vue.prototype.HOST = 'http://www.gamepaper.cn/'
 Vue.config.productionTip = false
--- src/pages/IndexTest.vue
+++ src/pages/IndexTest.vue
@@ -1,6 +1,36 @@
 <template>
   <div class="indexTest">
-    <h1>{{msg}}</h1>
+    <header class="header">
+      <img class="logo" src="../assets/images/index_logo.png" alt="">
+    </header>
+    <section class="main">
+      <div class="hbox">
+        <div class="pic character">
+          <a href="ph1/index.html"><button class="enter_button"></button></a>
+        </div>
+        <div class="pic weapon">
+          <a href="wea/index.html"><button class="enter_button"></button></a>
+        </div>
+      </div>
+      <div class="hbox">
+        <div class="pic stigmata">
+          <a href="sti/index.html"><button class="enter_button"></button></a>
+        </div>
+        <div class="pic goods">
+          <a href=""><button class="enter_button"></button></a>
+        </div>
+      </div>
+      <div class="sakura_bg_box box1" ref="box1"></div>
+      <div class="sakura_bg_box box2" ref="box2"></div>
+      <div class="sakura_bg_box box3" ref="box3"></div>
+      <div class="sakura_bg_box box4" ref="box4"></div>
+      <div class="sakura_bg_box box5" ref="box5"></div>
+    </section>
+    <footer class="footer">
+      <a class="link" href="">攻略文章</a>
+      <a class="link" href="">最新活动</a>
+      <a class="link" href="">关注我们</a>
+    </footer>
   </div>
 </template>
 
@@ -11,10 +41,125 @@ export default {
     return {
       msg: '情报姬首页'
     }
+  },
+  methods: {
+    initSakura: function () {
+      // console.log(this.$refs)
+      this.$snowFall.foo.snow(document.getElementsByClassName('box1'), {image: '/static/images/index_sakura1.png', minSize: 10, maxSize: 30, round: true})
+      this.$snowFall.foo.snow(document.getElementsByClassName('box2'), {image: '/static/images/index_sakura2.png', minSize: 10, maxSize: 30, round: true})
+      this.$snowFall.foo.snow(document.getElementsByClassName('box3'), {image: '/static/images/index_sakura3.png', minSize: 10, maxSize: 30, round: true})
+      this.$snowFall.foo.snow(document.getElementsByClassName('box4'), {image: '/static/images/index_sakura4.png', minSize: 10, maxSize: 30, round: true})
+      this.$snowFall.foo.snow(document.getElementsByClassName('box5'), {image: '/static/images/index_sakura5.png', minSize: 10, maxSize: 30, round: true})
+    }
+  },
+  mounted () {
+    this.initSakura()
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.indexTest {
+  // background-image: url(../assets/images/index_bg.png);
+  height: 17.90rem;
+  .header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #1e263b;
+    height: 1.76rem;
+    .logo {
+      height: 0.7rem;
+    }
+  }
+  .main {
+    .hbox {
+      padding: .53rem .53rem 0 .53rem;
+      display: flex;
+      .pic {
+        position: relative;
+        height: 6.51rem;
+        text-align: center;
+        flex: 1;
+        background-size: cover;
+        .enter_button {
+          position: absolute;
+          background-image: url(../assets/images/index_enter_dark.png);
+          background-size: cover;
+          background-color: transparent;
+          border: none;
+          top: 5.60rem;
+          left: 1.33rem;
+          height: .75rem;
+          width: 1.95rem;
+        }
+        .enter_button:hover {
+          background-image: url(../assets/images/index_enter_light.png);
+        }
+      }
+      .character {
+        background-image: url(../assets/images/index_character.png);
+        margin-right: .27rem;
+      }
+      .weapon {
+        background-image: url(../assets/images/index_weapon.png);
+        margin-left: .27rem;
+      }
+      .stigmata {
+        background-image: url(../assets/images/index_stigmata.png);
+        margin-right: .27rem;
+      }
+      .goods {
+        background-image: url(../assets/images/index_goods.png);
+        margin-left: .27rem;
+      }
+    }
+    .sakura_bg_box {
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      background-color: transparent;
+    }
+    .box1 {
+      z-index: -1;
+    }
+    .box2 {
+      z-index: -2;
+    }
+    .box3 {
+      z-index: -3;
+    }
+    .box4 {
+      z-index: -4;
+    }
+    .box5 {
+      z-index: -5;
+      background-image: url(../assets/images/index_bg.png);
+    }
+  }
+  .footer {
+    background-image: url(../assets/images/index_footer.png);
+    background-size: cover;
+    width: 10rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    height: 1.31rem;
+    margin: 0;
+    bottom: 0;
+    .link {
+      font-size: .48rem;
+      flex: 1;
+      text-align: center;
+      color: white;
+    }
 
+    .link:hover {
+      color: deepskyblue;
+    }
+  }
+}
 </style>
