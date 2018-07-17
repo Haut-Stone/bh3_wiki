bdbe2192f2ca93b254818f809e8cf30c44927326
SJH <haut.1604.stone@gmail.com>
2018-07-17 17:23:20 +0800

    从老代码中添加了ScrollMenu

---
+++ src/components/ScrollMenu.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="scroll_menu">
+    <ul class="scroll_nav">
+      <li v-for="item in items" :key="item.id">
+        <router-link :to="item.path">{{item.text}}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScrollMenu',
+  props: {
+    items: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+/* 滚动导航菜单 */
+.scroll_menu{
+    width: 10rem;
+    height: .667rem;
+    padding: .24rem 0;
+    background: #1c2b42;
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    z-index: 999;
+    .title{
+      height: .667rem;
+      line-height: .667rem;
+      padding: 0 .333rem;
+      font-size: .533rem;
+      color: #8e95a1;
+      border-right: 2px solid #0d1e38;
+    }
+    .scroll_nav{
+      flex: 1;
+      display: flex;
+      font-size: .427rem;
+      li{
+        height: .667rem;
+        line-height: .667rem;
+        flex: 1;
+        text-align: center;
+        a{
+          color: #fff;
+          &.active{
+            color: #ffb434;
+          }
+        }
+      }
+    }
+}
+</style>
--- src/pages/Stigmata/Stigmata.vue
+++ src/pages/Stigmata/Stigmata.vue
@@ -3,16 +3,19 @@
     <h1 v-if="$route.params.star">{{ $route.params.star }}</h1>
     <h1 v-else>{{ $route.params.id }}</h1>
     <NavMenu :items="navMenuItems" name="圣痕"></NavMenu>
+    <ScrollMenu v-show="scrollTop > 150" :items="navMenuItems"></ScrollMenu>
     <router-view/>
   </div>
 </template>
 
 <script>
 import NavMenu from '@/components/NavMenu'
+import ScrollMenu from '@/components/ScrollMenu'
 export default {
   name: 'Stigmata',
   data () {
     return {
+      scrollTop: 0,
       msg: '圣痕导航',
       navMenuItems: [
         {path: 'fiveStar', text: '五星', id: 1},
@@ -23,12 +26,28 @@ export default {
       ]
     }
   },
+  mounted () {
+    window.addEventListener('scroll', this._handleScroll)
+  },
+  methods: {
+    // 获取滚动条高度
+    _handleScroll () {
+      const scrollTop =
+        window.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop
+      this.scrollTop = scrollTop
+    }
+  },
   components: {
-    NavMenu
+    NavMenu,
+    ScrollMenu
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.stigmata {
+  height: 16rem;
+}
 </style>
