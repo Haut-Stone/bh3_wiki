1fdbacbd6dd88668cee7a624b61e658d4b9d4a20
SJH <haut.1604.stone@gmail.com>
2018-07-17 17:47:10 +0800

    添加了部分模块，星级，无数据，和块标题

---
+++ src/components/BlockTitle.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="block_title">
+    <div class="icon" :class="'block_title_icon' + icon"></div>
+    <div class="title">{{title}}</div>
+    <div class="right_text" v-if="rightTxt">{{rightTxt}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BlockTitle',
+  props: {
+    title: {
+      type: String,
+      default: '标题'
+    },
+    rightTxt: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: Number,
+      default: 1
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.block_title {
+  width: 9.333rem;;
+  margin: 0 auto;
+  height: 0.6rem;
+  line-height: 0.6rem;
+  padding-bottom: .107rem;
+  background: url(../../assets/images/block_title_border.png) no-repeat center bottom;
+  background-size: 9.333rem;
+  font-size: .373rem;
+  color: #1c2b42;
+  font-weight: bold;
+  display: flex;
+  .icon {
+    width: .64rem;
+    height: .56rem;
+    margin-right: .213rem;
+    &.blockTitleIcon1 {
+      background: url('../../assets/images/block_title_icon_1.png') no-repeat;
+      background-size: 0.64rem;
+    }
+    &.blockTitleIcon2 {
+      background: url('../../assets/images/block_title_icon_2.png') no-repeat;
+      background-size: 0.64rem;
+    }
+    &.blockTitleIcon3 {
+      background: url('../../assets/images/block_title_icon_3.png') no-repeat;
+      background-size: 0.64rem;
+    }
+  }
+  .title {
+    flex: 1;
+    color: #1c2b42;
+    font-weight: bold;
+  }
+  .rightTxt {
+    text-align: right;
+    margin-right: .133rem;
+    color: #9ca3ad;
+    font-size: .32rem;
+  }
+}
+</style>
---
+++ src/components/Star.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="star" :style="'width:' + this.starWidth"></div>
+</template>
+
+<script>
+export default {
+  name: 'Star',
+  props: {
+    star: {
+      type: Number,
+      default: 1
+    }
+  },
+  computed: {
+    starWidth () {
+      if (this.star > 5) {
+        return '1.533rem'
+      } else {
+        return this.star / 5 * 1.533 + 'rem'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.star{
+  width: 1.533rem;
+  height: .267rem;
+  background: url('../../assets/images/star.png') no-repeat;
+  background-size: 1.533rem .267rem;
+}
+</style>
